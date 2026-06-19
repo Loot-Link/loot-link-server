a5d5638a85e20885428f4ce6f0266c5e414eede9
Evan Miller-Jones <evan.millerjones@gmail.com>
2026-06-18 19:04:20 -0500

    fix connection URLs for production

--- api/raidhelper.js
+++ api/raidhelper.js
@@ -45,7 +45,7 @@ router.post("/import",  async (req, res, next) => {
 
 router.get("/imported",  async (req, res, next) => {
   try {
-    console.log("Logged in user:", req.user.user_id);
+    // console.log("Logged in user:", req.user.user_id);
     const raids = await getRaidHelperEventsByUserId();
     console.log("Raids found:", raids.length);
     res.send(raids);
